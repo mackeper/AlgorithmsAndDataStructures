2aefffa306640c12090f3afab1fab82d31f59e6e
Marcus Östling <marcus.ftw94@gmail.com>
2018-11-21 08:55:41 +0100

    differentdistances

---
+++ kattis/differentdistances.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    //For faster I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    double x1, x2, y1, y2, p;
+    for(cin >> x1; x1 != 0; cin >> x1) {
+        cin >> y1 >> x2 >> y2 >> p;
+        cout << pow(pow(abs(x1-x2),p)+pow(abs(y1-y2),p), 1/p) << "\n";
+    }
+
+    return 0;
+}
