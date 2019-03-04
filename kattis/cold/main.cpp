60da52c9be31808dbc86c9ccc679b9cdf1a391f2
Marcus Östling <mpt.ostling@gmail.com>
2019-03-04 08:38:07 +0100

    done

---
+++ kattis/cold/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define EPS 10e-9
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+    while(n--) {
+        int x;
+        cin >> x;
+        count = x < 0 ? count+1 : count;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
