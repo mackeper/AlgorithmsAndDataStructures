a8cc3f9e2e9d0a815f1162345ffd5023cce323a7
Marcus Östling <mpt.ostling@gmail.com>
2019-03-23 10:41:07 +0100

    comment exception

--- kattis/main.cpp
+++ kattis/main.cpp
@@ -12,9 +12,9 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
-  cin.sync_with_stdio(0);
-  cin.tie(0);
-  cin.exceptions(cin.failbit);
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    //cin.exceptions(cin.failbit);
 
-  return 0;
+    return 0;
 }
