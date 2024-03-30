dfac4937dac0b4db39c22d9c276af1de8224ecd6
Marcus Östling <mpt.ostling@gmail.com>
2024-03-30 08:07:45 +0100

    Start with binary heap

---
+++ datastructures/csharp/DataStructures.Tests/BinaryHeapTests.cs
@@ -0,0 +1,5 @@
+﻿namespace DataStructures.Tests;
+
+public class BinaryHeapTests {
+
+}
---
+++ datastructures/csharp/DataStructures/BinaryHeap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures;
+
+public class BinaryHeap {
+
+    private readonly List<int> heap;
+    public int Capacity { get; }
+    public int Size { get; private set; }
+
+    public BinaryHeap(int capacity) {
+        heap = new List<int>(capacity);
+        Capacity = capacity;
+    }
+
+    public bool Insert(int value) {
+        if (Size == Capacity) {
+            return false;
+        }
+
+        return true;
+    }
+}
